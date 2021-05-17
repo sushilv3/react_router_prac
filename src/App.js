c77c1f32ce777104cb68112dde1721e93ecc59c8
sushilv3 <sushilv3rma@gmail.com>
2021-05-17 15:29:33 +0530

    added Route and Swich components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import { Route, Switch } from "react-router-dom";
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Error from "./components/Error";
+import "./App.css";
 
-function App() {
+const App = () => {
+  const Name = () => {
+    return <>
+      <h3>Name page </h3>
+    </>;
+  };
   return (
     <>
-    <h1>App component</h1>
+      <Switch>
+        <Route exact path="/" component={About} />
+        <Route exact path="/contact" component={Contact} />
+        <Route exact path="/contact/name" component={Name} />
+        <Route component={Error} />
+      </Switch>
     </>
   );
-}
+};
 
 export default App;
