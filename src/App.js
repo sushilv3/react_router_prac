8827f1c9fb1407d72584020af126e411926b92da
sushilv3 <sushilv3rma@gmail.com>
2021-05-18 10:20:38 +0530

    use render and component pprops in Route component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import About from "./components/About";
 import Contact from "./components/Contact";
 import Error from "./components/Error";
 import NavBar from "./components/NavBar";
+import Service from './components/Service';
 import "./App.css";
 
 const App = () => {
@@ -18,8 +19,9 @@ const App = () => {
     <>
       <NavBar />
       <Switch>
-        <Route exact path="/" component={About} />
-        <Route exact path="/contact" component={Contact} />
+        <Route exact path="/" component={()=><About name="about porops"/>} />
+        <Route exact path="/contact" component={()=><Contact name="contact props"/>} />
+        <Route exact path="/service" render={()=><Service name="service props"/>} />
         <Route exact path="/contact/name" component={Name} />
         <Route component={Error} />
       </Switch>
