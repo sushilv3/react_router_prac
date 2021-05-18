178da75cb2a7846b46beadeaa6e900113bba110e
sushilv3 <sushilv3rma@gmail.com>
2021-05-18 10:58:31 +0530

    added a user Route

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Contact from "./components/Contact";
 import Error from "./components/Error";
 import NavBar from "./components/NavBar";
 import Service from './components/Service';
+import User from './components/User'
 import "./App.css";
 
 const App = () => {
@@ -22,6 +23,7 @@ const App = () => {
         <Route exact path="/" component={()=><About name="about porops"/>} />
         <Route exact path="/contact" component={()=><Contact name="contact props"/>} />
         <Route exact path="/service" render={()=><Service name="service props"/>} />
+        <Route exact path="/user/:fname" component={User} />
         <Route exact path="/contact/name" component={Name} />
         <Route component={Error} />
       </Switch>
