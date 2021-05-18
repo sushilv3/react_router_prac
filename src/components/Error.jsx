385f1f32acb80ec92e86e823d89a77e1d14cd4e8
sushilv3 <sushilv3rma@gmail.com>
2021-05-18 15:12:25 +0530

    added NavLink component into error page

--- src/components/Error.jsx
+++ src/components/Error.jsx
@@ -1,9 +1,12 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 
 const Error = () => {
   return (
     <>
       <h2>oops!!404 Page not found</h2>
+      <p>Sorry, this page doesn't exist</p>
+      <NavLink to='/'>Go back</NavLink>
     </>
   );
 };
