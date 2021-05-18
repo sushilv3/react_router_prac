c78436866123c829e7899607e2d75a7d04e91c72
sushilv3 <sushilv3rma@gmail.com>
2021-05-18 09:28:18 +0530

    added NavBar component

--- src/App.js
+++ src/App.js
@@ -3,16 +3,20 @@ import { Route, Switch } from "react-router-dom";
 import About from "./components/About";
 import Contact from "./components/Contact";
 import Error from "./components/Error";
+import NavBar from "./components/NavBar";
 import "./App.css";
 
 const App = () => {
   const Name = () => {
-    return <>
-      <h3>Name page </h3>
-    </>;
+    return (
+      <>
+        <h3>Name page </h3>
+      </>
+    );
   };
   return (
     <>
+      <NavBar />
       <Switch>
         <Route exact path="/" component={About} />
         <Route exact path="/contact" component={Contact} />
