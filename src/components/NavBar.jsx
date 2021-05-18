4813b937e2dc179270a7cbc2fae7ffe8e1b70705
sushilv3 <sushilv3rma@gmail.com>
2021-05-18 10:24:43 +0530

    added service component

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,12 +1,21 @@
 import React from "react";
-import {NavLink} from 'react-router-dom';
+import { NavLink } from "react-router-dom";
 
 const NavBar = () => {
   return (
     <>
-    <NavLink exact activeClassName="active_class" to="/">About us</NavLink>
-    <NavLink exact activeClassName="active_class" to="/contact">Contact us</NavLink>
-    <br />
+      <div className="menu_style">
+        <NavLink exact activeClassName="active_class" to="/">
+          About us
+        </NavLink>
+        <NavLink exact activeClassName="active_class" to="/service">
+          Service
+        </NavLink>
+        <NavLink exact activeClassName="active_class" to="/contact">
+          Contact us
+        </NavLink>
+      </div>
+      <br />
       <a href="/">About Us</a>
 
       <a href="/contact">contact Us</a>
