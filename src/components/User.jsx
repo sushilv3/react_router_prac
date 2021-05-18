4d5100a826a3125d3dac74e36e2f7cdb0eb21393
sushilv3 <sushilv3rma@gmail.com>
2021-05-18 11:31:28 +0530

    used useLocation hook

--- src/components/User.jsx
+++ src/components/User.jsx
@@ -1,15 +1,24 @@
 import React from "react";
-import { useParams } from "react-router-dom";
+import { useParams, useLocation } from "react-router-dom";
 
 // first way without using useParams hook
 // const User = ({ match }) => {
-  
 
 //   return <h2>User Component {match.params.fname}</h2>;
 // };
- const User = () => {
-  const {fname} = useParams();
+const User = () => {
+  const { fname } = useParams();
 
-   return <h2>User Component {fname}</h2>;
- };
+  const location = useLocation();
+
+  console.log(location);
+  return (
+    <>
+      <h2>User Component {fname}</h2>
+      <p>My cuurent location {location.pathname}</p>
+        {location.pathname === `/user/sushil` ? (<button onClick={()=>alert('u r awesome')}>click me</button>):null}
+
+    </>
+  );
+};
 export default User;
